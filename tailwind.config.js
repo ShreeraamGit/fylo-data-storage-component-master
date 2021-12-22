1ba8c94d17efc874c37e2ffe7f3a7b2404082688
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2021-12-22 12:08:56 +0100

    Desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'main': 'hsl(229, 57%, 11%)',
+                'box-bg': 'hsl(228, 56%, 26%)',
+                'small-box-bg': 'hsl(229, 7%, 55%)',
+                'color-from': 'hsl(6, 100%, 80%)',
+                'color-to': 'hsl(335, 100%, 65%)'
+            },
+            backgroundImage: {
+                'desktop-pattern': "url('/images/bg-desktop.png')",
+                'mobile-pattern': "url('/images/bg-mobile.png')",
+            },
+            borderRadius: {
+                'none': '0',
+                'sm': '0.125rem',
+                DEFAULT: '0.25rem',
+                DEFAULT: '4px',
+                'md': '0.375rem',
+                'lg': '0.5rem',
+                'full': '9999px',
+                'large': '120px',
+            },
+            fontFamily: {
+                'myfont': ['"Raleway", sans-serif']
+            },
+            fontSize: {
+                'mysize': '14px',
+            }
+        },
     },
     plugins: [],
 }
